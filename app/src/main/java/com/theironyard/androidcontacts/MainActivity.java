0003aba9914781e0529aed78a33ed16f13f59f4b
DannyLocke <dlocke@mac.com>
2017-01-23 14:35:23 -0600

    Initial commit

---
+++ app/src/main/java/com/theironyard/androidcontacts/MainActivity.java
@@ -0,0 +1,83 @@
+package com.theironyard.androidcontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
+
+    ArrayAdapter<String> contacts;
+
+    ListView list;
+    EditText name;
+    EditText number;
+    Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView);
+        name = (EditText) findViewById(R.id.editName);
+        number = (EditText) findViewById(R.id.editNumber);
+        addButton = (Button) findViewById(R.id.button2);
+
+        contacts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(contacts);
+
+        addButton.setOnClickListener(this);
+        list.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String contactName = name.getText().toString();
+        String contactNumber = number.getText().toString();
+
+        contacts.add(contactName + " (" + contactNumber + ")");
+
+        name.setText("");
+        number.setText("");
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+        String contact = contacts.getItem(position);
+        contacts.remove(contact);
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
