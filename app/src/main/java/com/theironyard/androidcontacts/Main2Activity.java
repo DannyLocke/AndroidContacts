b9aea768ca9f0184f026fefae5e62d77fa784d09
DannyLocke <dlocke@mac.com>
2017-01-24 15:30:42 -0600

    adding 2nd activity

--- app/src/main/java/com/theironyard/androidcontacts/Main2Activity.java
+++ app/src/main/java/com/theironyard/androidcontacts/Main2Activity.java
@@ -1,50 +1,52 @@
 package com.theironyard.androidcontacts;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity
+        //implements View.OnClickListener
+        {
 
-    ArrayAdapter<String> contacts;
+//    TextView contact;
+//    EditText contactNotes;
+//
+//    int position = 0;
 
-    ListView list;
-    EditText name;
-    EditText number;
-    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String contact = intent.getStringExtra(MainActivity.CONTACT_CARD);
+        TextView textView = new TextView(this);
+        textView.setTextSize(25);
+        textView.setText(contact);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main2);
+        layout.addView(textView);
     }
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.second_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public void onClick(View view) {
+//
+//        Intent returnIntent = new Intent();
+//        returnIntent.putExtra("contactNotes", contactNotes.getText().toString());
+//        returnIntent.putExtra("position", position);
+//        setResult(Activity.RESULT_OK, returnIntent);
+//        finish();
+//    }
 }
--- app/src/main/java/com/theironyard/androidcontacts/MainActivity.java
+++ app/src/main/java/com/theironyard/androidcontacts/MainActivity.java
@@ -1,7 +1,10 @@
 package com.theironyard.androidcontacts;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +14,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends Activity implements View.OnClickListener,
-        AdapterView.OnItemLongClickListener  {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener  {
 
     ArrayAdapter<String> contacts;
 
@@ -20,6 +30,9 @@ public class MainActivity extends Activity implements View.OnClickListener,
     EditText name;
     EditText number;
     Button addButton;
+
+    //public static final int NOTES_REQUEST = 1;
+    static final String CONTACT_CARD = "com.theironyard.androidcontacts.contact";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +45,12 @@ public class MainActivity extends Activity implements View.OnClickListener,
         addButton = (Button) findViewById(R.id.button2);
 
         contacts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        //loadContacts();
         list.setAdapter(contacts);
 
         addButton.setOnClickListener(this);
         list.setOnItemLongClickListener(this);
+        list.setOnItemClickListener(this);
     }
 
     @Override
@@ -47,14 +62,120 @@ public class MainActivity extends Activity implements View.OnClickListener,
 
         name.setText("");
         number.setText("");
+        //saveContacts();
     }
 
     @Override
     public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-        String contact = contacts.getItem(position);
-        contacts.remove(contact);
+        final String contact = contacts.getItem(position);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Contact");
+        builder.setMessage("Are you sure you want to remove this item?");
+        builder.setPositiveButton(android.R.string.ok, new AlertDialog.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                contacts.remove(contact);
+                //saveContacts();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new AlertDialog.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //do nothing
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
         return true;
+
     }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        String contact = contacts.getItem(position);
+        intent.putExtra(CONTACT_CARD, contact);
+        startActivity(intent);
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if(requestCode == NOTES_REQUEST) {
+//            if(resultCode == RESULT_OK) {
+//                int position = data.getIntExtra("position", 0);
+//                String note = data.getStringExtra("contactNotes");
+//                saveContacts();
+//            }
+//        }
+//    }
+
+//    private void saveContacts() {
+//        try {
+//            FileOutputStream fos = openFileOutput(CONTACT_CARD, Context.MODE_PRIVATE);
+//
+//            StringBuilder sb = new StringBuilder();
+//            for(int i = 0; i < contacts.getCount(); i++) {
+//                String contact = contacts.getItem(i);
+//                String note = "";
+//                sb.append(contact + "," + note + "\n");
+//            }
+//            fos.write(sb.toString().getBytes());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+//    private void loadContacts() {
+//        try {
+//            FileInputStream fis = openFileInput(CONTACT_CARD);
+//            InputStreamReader isr = new InputStreamReader(fis);
+//            BufferedReader br = new BufferedReader(isr);
+//            int position = 0;
+//            while(br.ready()) {
+//                String contactLine = br.readLine();
+//                String lineParts[] = contactLine.split(",");
+//                if(lineParts.length > 0) {
+//                    contacts.add(lineParts[0]);
+//                    if (lineParts.length > 1) {
+//                        contacts.add(lineParts[1]);
+//                    }
+//                }
+//                position++;
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
